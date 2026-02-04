7d22051f8714f3ab322b5d8d92d75d386651bd9a
abhpandey <abhishekpaney@gmail.com>
2026-02-04 14:13:24 +0545

    updated

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Force Turbopack to use this folder as the workspace root
+  turbopack: {
+    root: ".",
+  },
 };
 
 export default nextConfig;
