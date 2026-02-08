137c3028fe3e25375f709f68caaa454347ae4f19
abhpandey <abhishekpaney54@gmail.com>
2026-02-08 10:54:00 +0545

    updated routes

--- backend/src/routes/auth.route.ts
+++ backend/src/routes/auth.route.ts
@@ -8,4 +8,6 @@ const router = Router();
 router.post("/register", authController.register)
 router.post("/login", authController.login)
 router.post("/logout", authController.logout)
+router.post("/forgot-password", authController.forgotPassword)
+router.post("/reset-password", authController.resetPassword)
 export default router;
