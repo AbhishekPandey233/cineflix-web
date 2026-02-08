61fe6995254bf8034aa1382e28490bd429c10ad2
abhpandey <abhishekpaney54@gmail.com>
2026-02-08 10:56:09 +0545

    modified for forgot password and reset password

--- frontend/lib/api/endpoints.ts
+++ frontend/lib/api/endpoints.ts
@@ -2,6 +2,8 @@
 export const API = {
     AUTH: {
         REGISTER: "/api/auth/register",
-        LOGIN: "/api/auth/login"
+        LOGIN: "/api/auth/login",
+        FORGOT_PASSWORD: "/api/auth/forgot-password",
+        RESET_PASSWORD: "/api/auth/reset-password",
     }
 }
